a91e19bfb2e3fa76ba92d7c663892a033be240ab
chames@gmail.com <maazkamran72@gmail.com>
2023-11-16 22:23:04 +0500

    saved cart items

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,15 @@ import { Store } from '@/utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
-import { useContext } from 'react';
+import { useContext,useEffect,useState } from 'react';
 
 export default function Layout({ title, children }) {
   const {state}=useContext(Store)
   const {cart}=state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
   return (
     <>
       <Head>
@@ -24,11 +28,15 @@ export default function Layout({ title, children }) {
 
    
     <Link href="/cart" className='p-2'>Cart
-    {cart.cartItems.length > 0 && (
-                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                    </span>
-                  )}</Link>
+   
+         
+                  {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                  )}
+
+                </Link>
     <Link href="/login" className='p-2'>Login</Link>
     </div>
 
--- utils/Store.js
+++ utils/Store.js
@@ -1,8 +1,12 @@
 import { createContext,useReducer } from "react";
+import Cookies from "js-cookie";
 export const Store=createContext();
 const InitialState={
-    cart:{cartItems:[]},
-};
+    cart: Cookies.get('cart')
+    ? JSON.parse(Cookies.get('cart'))
+    : { cartItems: [] },
+}
+
 function reducer(state,action)
 {
     switch(action.type){
@@ -14,6 +18,7 @@ function reducer(state,action)
             const cartItems=existItem?state.cart.cartItems.map((item)=>
             item.name===existItem.name?newItem:item):
             [...state.cart.cartItems,newItem];
+            Cookies.set('cart',JSON.stringify({...state.cart,cartItems}))
             return { ...state, cart: { ...state.cart, cartItems } };
 
         }
